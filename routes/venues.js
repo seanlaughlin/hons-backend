c9b8e40349ea00fbb5a0c10059457bd990a0ba7b
seanlaughlin <71667909+seanlaughlin@users.noreply.github.com>
2024-03-10 16:21:02 +0000

    Update venues.js test

--- routes/venues.js
+++ routes/venues.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
 // only filters if does have one of accessibiltiies supplied - doesn't filter out if doesn't have others
 router.post("/filter", async (req, res) => {
   const location = req.body.location;
-  const maxDistance = req.body.maxDistance;
+  const maxDistance = 5;
   const categoryIds = req.body.categoryIds;
   const accessibilityCriteria = req.body.accessibilityCriteria;
   const searchTerm = req.body.searchTerm;
